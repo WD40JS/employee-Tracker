980b15a9aec27e742014a8eb7896b113e312e150
Devon Weems <devonweems@Devons-MacBook-Air.local>
2020-09-05 19:32:41 -0400

    Working Application
    Application now works without disfunction or interruptions.

---
+++ assests/employees.sql
@@ -0,0 +1,56 @@
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
+USE employees;
+CREATE TABLE department (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) UNIQUE NOT NULL
+);
+CREATE TABLE role (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL UNSIGNED NOT NULL,
+  department_id INT UNSIGNED NOT NULL,
+  INDEX dep_ind (department_id),
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
+CREATE TABLE employee (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT UNSIGNED NOT NULL,
+  INDEX role_ind (role_id),
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+  manager_id INT UNSIGNED,
+  INDEX man_ind (manager_id),
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
+use employees;
+INSERT INTO department
+    (name)
+VALUES
+    ('Operations'),
+    ('Analystics'),
+    ('Marketing'),
+    ('Executive');
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('General Manager', 11000000, 1),
+    ('Coach', 4000000, 1),
+    ('Team Lead Analyst', 15000000, 2),
+    ('Team Analyst', 8000000, 2),
+    ('Media Manager', 7000000, 3),
+    ('Media Specialist', 3000000, 3),
+    ('CEO', 45000000, 4),
+    ('CEO Assistant', 25000000, 4);
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('John', 'Smith', 1, NULL),
+    ('Carl', 'Sagan', 2, 1),
+    ('Johnathan', 'Wick', 3, NULL),
+    ('Jason', 'Borne', 4, 3),
+    ('Ethan', 'Hunt', 5, NULL),
+    ('Samo', 'Huang', 6, 5),
+    ('Jacki', 'Chan', 7, NULL),
+    ('Jet', 'Li', 8, 7);
